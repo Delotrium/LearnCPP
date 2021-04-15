3b699162e50e5b10d90fc74dfc9dceaca92791f5
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2021-04-15 18:19:40 +1000

    Hello World!
    Simple program that prints "Hello World" in terminal.

---
+++ One.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // Include the library/header to in/out stream (e.i input and output in terminal)
+
+int main() //The main function
+{
+	std::cout << "Hello World"; //Print "Hello World" using cout from the standard library
+	return 0; //Return code 0
+}
