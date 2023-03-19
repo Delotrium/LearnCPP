7aec5c182163b47f1136096d888c147b7a5ec5d0
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2023-03-19 10:16:22 +1000

    Operators

---
+++ Code/10-19/Learn_12.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+void operators() {
+	int x = 5 + 5; //Addition - 10
+	x = 5 - 5; //Substraction - 0
+	x = 5 * 5; //Multiplication - 25
+	x = 5 / 5; //Division - 1 (Integer division)
+	x = 5. / 5; //Division - 1 (double division)
+	x = 5 % 4; //Modulus Operator
+
+}
+
+
+int main() {
+
+}
