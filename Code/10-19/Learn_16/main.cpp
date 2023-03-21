fc7df7f442925546e0decc0863e6cfc4abf089c7
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2023-03-21 14:50:23 +1000

    Multiple files

---
+++ Code/10-19/Learn_16/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int add(int x, int y); //Tell the program the the "add" function exists somewhere in the project; so that it knows it exists
+
+int main() {
+	std::cout << add(1, 2);
+
+}
